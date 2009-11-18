43bc4f744eea8f80b1a4b160f046d5adecb59713
Juan P. Daza P <juan.daza@er-technology.net>
2009-11-18 13:15:07 -0500

    License information

--- socketgo.go
+++ socketgo.go
@@ -1,3 +1,7 @@
+// Copyright 2009 ER Technology Ltda. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
