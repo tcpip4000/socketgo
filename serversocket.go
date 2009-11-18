642afc5adb6c0b5f187f00b47bca33340873028e
Juan P. Daza P <juan.daza@er-technology.net>
2009-11-17 21:10:06 -0500

    Making a go socket server

--- serversocket.go
+++ serversocket.go
@@ -8,15 +8,25 @@ import (
 
 func main() {
 	var host = "127.0.0.1";
-	var port = "8764";
+	var port = "9998";
 	var remote = host + ":" + port;
+	//var data []byte;
+	var data2 = make([]byte, 1024);
 
 	lis, error := net.Listen("tcp", remote);
+	defer lis.Close();
 	if error != nil { fmt.Printf("Error creating listener: %s\n", error ); os.Exit(1); }
 	for {
+		con, error := lis.Accept();
+		defer con.Close();
+		if error != nil { fmt.Printf("Error accepting: %s\n", error); os.Exit(2); }
+		for {
+			n, error := con.Read(data2);
+			if error != nil { fmt.Printf("Error reading: %s, %d", error, n); os.Exit(3); }
+			fmt.Println(string(data2));
+		}
 	}
 
-	lis.Close();
-	fmt.Println("Listener closing.. ");
+	fmt.Println("Closing server...");
 }
 
